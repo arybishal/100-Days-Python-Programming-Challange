75a7240ccde99dc25f35efdb8f3b73d769c27ed0
Bishal Aryal <arybishal@gmail.com>
2023-08-31 17:57:26 +0400

    day 4

---
+++ Day 4 Comments, Escape Sequences & Print Statement/Single-Line Comments.py
@@ -0,0 +1,14 @@
+#Hey Bishal is working on this line , please do not remove
+#author: Bishal Aryal
+#version: 0.01
+print   (" hey we are learning Python today")
+
+#print   (" hey we are learning 
+#         Python today")      (this will give error)
+#to slove this add \n 
+print   (" hey we are\nand learning Python today")
+
+#another example
+print ("first section of text")
+print("\n\n")  #you can use as many as you can to create space
+print("second section of text")
---
+++ Day 4 Comments, Escape Sequences & Print Statement/main.py
@@ -0,0 +1,40 @@
+#Single-Line Comments:
+
+#Example 1
+#This is a 'Single-Line Comment'
+print("This is a print statement.")
+
+#Example 2
+print("Hello World !!!") #Printing Hello World
+
+#Example 3
+print("Python Program")
+#print("Python Program")
+
+#Multi-Line Comments:
+
+# Example 1: The use of ‘#’.
+# It will execute a block of code if a specified condition is true.
+# If the condition is false then it will execute another block of code.
+p = 7
+if (p > 5):
+    print("p is greater than 5.")
+else:
+    print("p is not greater than 5.")
+    
+#Example 2: The use of multiline string.
+"""This is an if-else statement.
+It will execute a block of code if a specified condition is true.
+If the condition is false then it will execute another block of code."""
+p = 7
+if (p > 5):
+    print("p is greater than 5.")
+else:
+    print("p is not greater than 5.")
+    
+    
+#Escape Sequence Characters
+
+# print("This doesnt "execute")
+# print("This will \" execute")
+
